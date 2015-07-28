46d873a8847961e296f21077ecd4555b47ea962b
Ben Whittle <benwhittle31@gmail.com>
2015-07-28 01:53:53 -0700

    Sexy ass slides with positioning

--- app/assets/javascripts/views/home/lang-selection-list.js
+++ app/assets/javascripts/views/home/lang-selection-list.js
@@ -15,12 +15,24 @@ define(['jquery',
 
 		initialize: function () {
             this.LANG_ITEMS = [];
+            this.baseX = -1*((window.innerWidth/2)-25);
+            this.itemDiameter = 50;
 		},
 
 		events: {
         },
 
+        getItemSlidePos: function () {
+            var itemLength = this.LANG_ITEMS.length;
+            var roundedDisplacement = Math.ceil(itemLength/2);
+            var even = itemLength % 2;
+            var side = even ? -1 : 1;
+            var newRight = even ? this.baseX + ((roundedDisplacement+itemLength) * this.itemDiameter) : this.baseX - ((roundedDisplacement-itemLength) * this.itemDiameter);
+            return newRight;
+        },
+
         addItem: function () {
+            var newRight = this.getItemSlidePos();
             var langSelection = new LangSelectionView({
                 tagName: 'li'
             });
@@ -28,18 +40,18 @@ define(['jquery',
             this.prepareItemForEntrance(langSelection.el);
             this.$el.find('.lang-selection-list').append(langSelection.el);
             this.LANG_ITEMS.push(langSelection);
-            this.slideItemIn(langSelection.el);
+            this.slideItemIn(langSelection.el, newRight);
         },
 
         prepareItemForEntrance: function (el) {
             el.style.display = 'inline-block';
             el.style.listStyleType = 'none';
             el.style.position = 'relative';
-            el.style.right = -1 * (window.innerWidth - (this.$el.find('.lang-selection-list').length * 50)) + 'px';
+            el.style.right = -1 * (window.innerWidth - (this.$el.find('.lang-selection-list').length * this.itemDiameter)) + 'px';
         },
 
-        slideItemIn: function (el) {
-            $(el).velocity({ right: 0}, 900, [100, 15]);
+        slideItemIn: function (el, newRight) {
+            $(el).velocity({ right: newRight}, 900, [100, 15]);
         },
 
 		render: function () {
