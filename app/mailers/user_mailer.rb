d5b5ec2800d072d216f0e1db2f2b323c7fefdb8e
Ben Whittle <benwhittle31@gmail.com>
2015-10-04 16:34:50 -0700

    cleaned up the confusion around whether or not I need to explicitly define the layout param at the top of user mailer and where the email view template actually goes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: 'benwhittle31@gmail.com',
           return_path: 'benwhittle31@gmail.com'
 
-  layout 'project_invitation'
-
   def project_invitation(email, name, subject)
     @name = name
-    mail = mail(to: email, subject: subject)
-    mail.deliver
+    mail(to: email, subject: subject)
   end
 end
