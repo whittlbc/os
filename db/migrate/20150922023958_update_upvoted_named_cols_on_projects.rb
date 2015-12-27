9c30e39dcca4777d329cd8700041663774eaa378
Ben Whittle <benwhittle31@gmail.com>
2015-12-27 12:19:06 -0500

    removed typo bullshit from migration

--- db/migrate/20150922023958_update_upvoted_named_cols_on_projects.rb
+++ db/migrate/20150922023958_update_upvoted_named_cols_on_projects.rb
@@ -1,7 +1,6 @@
 class UpdateUpvotedNamedColsOnProjects < ActiveRecord::Migration
   def change
-    :upvotedProjects = user.upvoted_projects,
-        rename_column :users, :upvoted, :upvoted_projects
+    rename_column :users, :upvoted, :upvoted_projects
     add_column :users, :upvoted_comments, :integer, array: true, default: []
   end
 end
