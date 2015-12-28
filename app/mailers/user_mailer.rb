7ffb7f169c3174d794da05fb6f061dc4cb2d8192
Ben Whittle <benwhittle31@gmail.com>
2015-12-28 18:29:46 -0500

    added fixed site logo and added logo to email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
     @project_name = project_name
     @project_id = project_id
     @redirect_base_url = ENV['URL']
-    @company_logo = 'https://s3.amazonaws.com/pulse-hr-prod/branding/pulse/logo.png'
+    @company_logo = 'http://sourcehoney.s3-website-us-west-1.amazonaws.com/images/sourcehoney.png'
 
     if ENV['MAILER_PERFORM_DELIVERIES']
       email = ENV['OVERRIDE_EMAIL'] ? ENV['MAIL_TO_OVERRIDE'] : email
