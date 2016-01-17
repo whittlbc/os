e6772aca17c8cf1659db35be5ed67f86368387f7
Ben Whittle <benwhittle31@gmail.com>
2016-01-16 20:56:16 -0800

    changed up some tutorial text and added special safari styling support

--- app/assets/javascripts/views/tutorial/project-type-item.js
+++ app/assets/javascripts/views/tutorial/project-type-item.js
@@ -16,9 +16,9 @@ define(['jquery',
       options = options || {};
 
       var textToTypeMap = {
-        'up-for-grabs': 'Here you will find ideas for projects posted by people who think something should be built, but just don\'t have the time to do it themselves. All of these are up for grabs.',
-        'on-the-fence': 'Here you will find posts made by people who are on the fence about starting a project (want some feedback, looking to form a team first, etc.). This is a good place for determining community interest in proposed projects before taking the time to actually build them.',
-        'launched': 'Here you will find projects that have already been launched, but whose maintainer is looking for more users and/or more contributors.'
+        'up-for-grabs': 'Here you will find ideas for projects posted by people who think something should be built but just don\'t have the time to do it themselves. All of these are up for grabs.',
+        'on-the-fence': 'Here you will find posts made by people who are on the fence about starting a project (want some feedback, looking to form a team first, etc.). This is a good place for gauging community interest before taking the time to actually build out a project.',
+        'launched': 'Here you will find projects that have already been launched, but whose maintainer is either looking for more users or more contributors.'
       };
 
       this.text = textToTypeMap[options.type];
