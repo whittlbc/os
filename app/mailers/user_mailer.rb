f0b6b1cc96f3d8cb182293acdce6a5b029b1c154
Ben Whittle <benwhittle31@gmail.com>
2016-02-16 22:06:41 -0800

    email bug fix again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,7 @@ class UserMailer < ActionMailer::Base
     @redirect_base_url = ENV['URL']
     @company_logo = LOGO
 
-    send(email, subject)
+    send_email(email, subject)
   end
 
   def notify_user_of_comment_reply(user: nil, comment: nil, parent_comment: nil, project: nil)
@@ -26,7 +26,7 @@ class UserMailer < ActionMailer::Base
     @company_logo = LOGO
     project_name_abbrev = @project_name.length > 40 ? "#{@project_name[0..37]}..." : @project_name
 
-    send(user.email, "Someone replied to your comment on the project: #{project_name_abbrev}")
+    send_email(user.email, "Someone replied to your comment on the project: #{project_name_abbrev}")
   end
 
   def notify_user_of_comment(user: nil, comment: nil, project: nil)
@@ -39,14 +39,14 @@ class UserMailer < ActionMailer::Base
     @company_logo = LOGO
     project_name_abbrev = @project_name.length > 40 ? "#{@project_name[0..37]}..." : @project_name
 
-    send(user.email, "Someone commented on your project: #{project_name_abbrev}")
+    send_email(user.email, "Someone commented on your project: #{project_name_abbrev}")
   end
 
   def received_suggestion(user)
     @recipient_name = user.gh_username
     @company_logo = LOGO
 
-    send(user.email, 'Thanks for the suggestion!')
+    send_email(user.email, 'Thanks for the suggestion!')
   end
 
   def notify_admin_of_suggestion(user: nil, text: nil)
@@ -55,10 +55,10 @@ class UserMailer < ActionMailer::Base
     @redirect_base_url = ENV['URL']
     @company_logo = 'http://sourcehoney.s3-website-us-west-1.amazonaws.com/images/sourcehoney.png'
 
-    send(ENV['ADMIN_EMAIL'], 'New Suggestion!')
+    send_email(ENV['ADMIN_EMAIL'], 'New Suggestion!')
   end
 
-  def send(email, subject)
+  def send_email(email, subject)
     if ENV['MAILER_PERFORM_DELIVERIES'] == 'true'
       mail(to: ((ENV['OVERRIDE_EMAIL'] == 'true') ? ENV['MAIL_TO_OVERRIDE'] : email), subject: subject)
     end
