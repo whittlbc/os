3d4f64c9a4bec0fb26f0eb4a37f011bf47eefcfb
Ben Whittle <benwhittle31@gmail.com>
2016-02-20 14:27:38 -0800

    changed some text

--- app/assets/javascripts/views/tutorial/project-type-item.js
+++ app/assets/javascripts/views/tutorial/project-type-item.js
@@ -16,7 +16,7 @@ define(['jquery',
       options = options || {};
 
       var textToTypeMap = {
-        'ideas': 'Here you will find a collection of project ideas formed by the community. Some are posted by members seeking feedback or initial teammates, while others are completely Up for Grabs (aka. take it and run with it).',
+        'ideas': 'Here you will find a collection of project ideas formed by the community. Some ideas are posted by members seeking feedback or initial teammates to help get their projects off the ground, while other ideas are completely Up for Grabs (a.k.a. take it and run with it).',
         'launched': 'Here you will find projects that have already been launched and are now seeking support in many possible ways (feedback, more users, more contributors, new maintainer, etc.)'
       };
 
