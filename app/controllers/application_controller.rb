f616a7fce76315734ab196fb68b26637c578d186
Ben Whittle <benwhittle31@gmail.com>
2016-02-20 15:16:31 -0800

    encoding explicitly to utf-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_headers
+
   def not_found
     redirect_to '/#ideas'
+  end
+
+  def set_headers
+    headers['Content-Type'] = 'text/html; charset=utf-8'
   end
 
 end
